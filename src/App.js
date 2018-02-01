a42e813051684ab8156106b3b21efda26a8d616f
Fprince333 <lwilsonsmith@gmail.com>
2018-02-01 16:20:41 -0500

    Add HTML and CSS to app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,155 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <div className='mini'>
+            <p className='products'>Empty</p>
+            <p className='names'></p>
+            <p className='miniprice'></p>
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>Webshop</li>
+            <li>Contact</li>
+          </ul>
+          <div className='cart icon-basket'>
+            <span className='number'>1</span>
+          </div>
+        </nav>
+        <div className='container'>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+          <div className='product'>
+            <img alt='' src='https://placeimg.com/200/100'/>
+            <h2 className='header'>Product Name</h2>
+            <p className='description'>Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque. Curabitur consectetur non ipsum in eleifend. Praesent id velit in nisi maximus porta nec vitae odio. Proin vitae magna a massa accumsan venenatis. Donec semper, sem in ullamcorper bibendum, mauris sem imperdiet lorem, tempor aliquet ligula lorem sit amet nibh. Suspendisse potenti.</p>
+            <p className='price'>231,-</p>
+            <div className='btn'>Add to cart</div>
+            <div className='quickview'>Quickview</div>
+          </div>
+        </div>
+        <div className='quickviewContainer'>
+          <div className='close'></div>
+          <h2 className='headline'></h2>
+          <p className='description'></p>
+          <img alt='' src='https://placeimg.com/100/100'/>
+          <img alt='' src='https://placeimg.com/100/100'/>
+          <img alt='' src='https://placeimg.com/100/100'/>
+        </div>
       </div>
     );
   }
