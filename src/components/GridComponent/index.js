004ff76a90985311721cc0dabe9adcebe7d9a5d3
Fprince333 <lwilsonsmith@gmail.com>
2018-02-21 16:59:31 -0500

    Add missing  Grid component elements

--- src/components/GridComponent/index.js
+++ src/components/GridComponent/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Row from '../layout/Row';
 import Column from '../layout/Column';
 import Gutter from '../layout/Gutter';
+import Padding from '../layout/Padding';
 
 const GridComponent = () => {
   return <Row title="Grid">
@@ -53,6 +54,20 @@ const GridComponent = () => {
         <Column>
           <div className="App-column-box" />
         </Column>
+      </div>
+      <div className="App-flexbox">
+        <Padding amount="20px 120px 0 20px">
+          <h5 className="App-featured-ctas">Grid</h5>
+          <p style={{ fontWeight: "bold" }} className="text-grey">
+            1120px
+          </p>
+        </Padding>
+        <Padding amount="20px 0 0 20px">
+          <h5 className="App-featured-ctas">Gutter</h5>
+          <p style={{ fontWeight: "bold" }} className="text-grey">
+            32px
+          </p>
+        </Padding>
       </div>
     </Row>;
 }
